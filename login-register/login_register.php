eb5689c89c8b08f266848d4d7bfb9c5dbfea9d76
Gaurav <gauravsarage.1902@gmail.com>
2023-07-19 21:06:58 +0530

    added code to check if the user can login after registering successfully, encrypted the password using blowfish algo

--- login-register/login_register.php
+++ login-register/login_register.php
@@ -3,7 +3,33 @@
 require('connection.php');
 
 # for login
+if (isset($_POST['login']))
+{
+    $query = "SELECT * FROM `registered_users` WHERE `email` = '$_POST[email_username]' OR `username` = '$_POST[email_username]'";
+    $result = mysqli_query($conn, $query);
 
+    if ($result) {
+        if (mysqli_num_rows($result) == 1) {
+            $result_fetch = mysqli_fetch_assoc($result);
+        } 
+        else {
+            echo"
+                <script>
+                    alert('Email or Username Not Registered');
+                    window.location.href = 'index.php'; 
+                </script>
+            ";
+        }
+    }
+    else {
+        echo"
+            <script>
+                alert('Cannot Run Query');
+                window.location.href = 'index.php'; 
+            </script>
+        ";
+    }
+}
 
 # for registration
 if (isset($_POST['register']))
@@ -37,7 +63,9 @@ if (isset($_POST['register']))
         }
         else # this query will be executed when no one has taken username or email before
         {
-            $query = "INSERT INTO `registered_users`(`full_name`, `user_name`, `email`, `password`) VALUES ('$_POST[fullname]','$_POST[username]','$_POST[email]','$_POST[password]')";
+            $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+            $query = "INSERT INTO `registered_users`(`full_name`, `user_name`, `email`, `password`) VALUES ('$_POST[fullname]','$_POST[username]','$_POST[email]','$password')";
 
             if(mysqli_query($conn, $query)) {
                 # if data inserted successfully
