7c6069eb84e8d7d9740c320ac48d92bb25ada0b6
Gaurav <gauravsarage.1902@gmail.com>
2023-07-19 19:16:01 +0530

    finished styling login page and started with register page

--- login-register/index.php
+++ login-register/index.php
@@ -4,8 +4,71 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Register Form</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    
+    <header>
+        <h2>Website Name</h2>
+        <nav>
+            <a href="index.php">HOME</a>
+            <a href="index.php">BLOG</a>
+            <a href="index.php">CONTACT</a>
+            <a href="index.php">ABOUT</a>
+        </nav>
+        <div class="sign-in-up">
+            <button type="button">LOGIN</button>
+            <button type="button">REGISTER</button>
+        </div>
+    </header>
+
+    <div class="popup-container">
+        <div class="popup">
+            <form>
+                <h2>
+                    <span>User Login</span>
+                    <button type="reset">X</button>
+                </h2>
+                <input 
+                    type="text" 
+                    placeholder="Enter your E-mail or username"
+                >
+                <input 
+                    type="password" 
+                    placeholder="Enter your password"
+                >
+                <button type="submit" class="login-btn">Login</button>
+            </form>
+        </div>
+    </div>
+
+
+    <div class="popup-container">
+        <div class="popup">
+            <form>
+                <h2>
+                    <span>User Register</span>
+                    <button type="reset">X</button>
+                </h2>
+                <input 
+                    type="text" 
+                    placeholder="Enter your Full Name"
+                >
+                <input 
+                    type="text"
+                    placeholder="Enter your Username"
+                
+                >
+                <input 
+                    type="email" 
+                    placeholder="Enter your E-mail"
+                >
+                <input 
+                    type="password"
+                    placeholder="Enter your Password"
+                >
+                <button type="submit" class="register-btn">Register</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
