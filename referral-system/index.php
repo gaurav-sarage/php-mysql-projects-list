0a80e9781fb20ea8d9b687a0ac6745739a8a921b
Gaurav <gauravsarage.1902@gmail.com>
2023-07-20 15:28:21 +0530

    added code files from login-register folder

---
+++ referral-system/index.php
@@ -0,0 +1,162 @@
+<?php 
+    require('connection.php');
+    session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Register Form</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h2>
+            Website Name
+        </h2>
+        <nav>
+            <a href="index.php">HOME</a>
+            <a href="index.php">BLOG</a>
+            <a href="index.php">CONTACT</a>
+            <a href="index.php">ABOUT</a>
+        </nav>
+        <?php
+            if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true)
+            {
+                echo"
+                    <div class='user'>
+                        $_SESSION[username] - <a href='logout.php'>
+                            LOGOUT
+                        </a>
+                    </div>                
+                ";
+            }
+            else {
+                echo"
+                    <div class='sign-in-up'>
+                        <button type='button' onclick=\"popup('login-popup')\">
+                            LOGIN
+                        </button>
+                        <button type='button' onclick=\"popup('register-popup')\">
+                            REGISTER
+                        </button>
+                    </div>
+                ";
+            }
+            
+
+        ?>
+
+    </header>
+
+    <div class="popup-container" id="login-popup">
+        <div class="popup">
+            <form  
+                method="POST"
+                action="login_register.php"
+            >
+                <h2>
+                    <span>User Login</span>
+                    <button 
+                        type="reset" 
+                        onclick="popup('login-popup')"
+                    >
+                        X
+                    </button>
+                </h2>
+
+                <input 
+                    type="text" 
+                    placeholder="Enter your E-mail or username"
+                    name="email_username"
+                >
+                <input 
+                    type="password" 
+                    placeholder="Enter your password"
+                    name="password"
+                >
+                <button 
+                    type="submit"
+                    class="login-btn"
+                    name="login"
+                >
+                    Login
+                </button>
+            </form>
+        </div>
+    </div>
+
+
+    <div class="popup-container" id="register-popup">
+        <div class="register popup">
+            <form method="POST" action="login_register.php">
+                <h2>
+                    <span>User Register</span>
+                    <button 
+                        type="reset"
+                        onclick="popup('register-popup')"
+                    >
+                        X
+                    </button>
+                </h2>
+                <input 
+                    type="text" 
+                    placeholder="Enter your Full Name"
+                    name="fullname"
+                >
+                <input 
+                    type="text"
+                    placeholder="Enter your Username"
+                    name="username"
+                >
+                <input 
+                    type="email" 
+                    placeholder="Enter your E-mail"
+                    name="email"
+                >
+                <input 
+                    type="password"
+                    placeholder="Enter your Password"
+                    name="password"
+                >
+                <button 
+                    type="submit" 
+                    class="register-btn"
+                    name="register"
+                >
+                    Register
+                </button>
+            </form>
+        </div>
+    </div>
+
+    <?php
+        if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true)
+        {
+            echo"
+                <h1
+                    style='text-align: center; margin-top: 200px;'
+                >
+                    Welcome to the website - $_SESSION[username]                 
+                </h1>
+            ";
+        }
+    ?>
+
+
+    <script>
+        function popup(popup_name) {
+            get_popup = document.getElementById(popup_name);
+            if (get_popup.style.display == "flex") {
+                get_popup.style.display = "none";
+            } else {
+                get_popup.style.display = "flex";
+            }
+        }
+    </script>
+
+</body>
+</html>
---
+++ referral-system/login_register.php
@@ -0,0 +1,118 @@
+<?php 
+
+require('connection.php');
+
+session_start();
+
+# for login
+if (isset($_POST['login']))
+{
+    $query = "SELECT * FROM `registered_users` WHERE `email` = '$_POST[email_username]' OR `user_name` = '$_POST[email_username]'";
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+        if (mysqli_num_rows($result) == 1) {
+            $result_fetch = mysqli_fetch_assoc($result);
+            if (password_verify($_POST['password'], $result_fetch['password'])) {
+                # if password metched
+                $_SESSION['logged_in'] = true;
+                $_SESSION['username'] = $result_fetch['user_name'];
+                header("location: index.php");   
+            }
+            else {
+                # if incorrect password
+                echo"
+                    <script>
+                        alert('Incorrect password');
+                        window.location.href = 'index.php'; 
+                    </script>
+                ";
+            }
+        } 
+        else {
+            echo"
+                <script>
+                    alert('Email or Username Not Registered');
+                    window.location.href = 'index.php'; 
+                </script>
+            ";
+        }
+    }
+    else {
+        echo"
+            <script>
+                alert('Cannot Run Query');
+                window.location.href = 'index.php'; 
+            </script>
+        ";
+    }
+}
+
+# for registration
+if (isset($_POST['register']))
+{
+    $user_exist_query = "SELECT * FROM `registered_users` WHERE `user_name` = '$_POST[username]' OR `email` = '$_POST[email]'";
+
+    $result = mysqli_query($conn, $user_exist_query);
+
+    if ($result) {
+        if (mysqli_num_rows($result) > 0) {
+            # if any user has already taken username or email
+            $result_fetch = mysqli_fetch_assoc($result);
+            if ($result_fetch['user_name'] == $_POST['username']){
+                # error for username already registered
+                echo"
+                    <script>
+                        alert('$result_fetch[user_name] - Is Already Taken');
+                        window.location.href = 'index.php'; 
+                    </script>
+                ";
+            }
+            else {
+                # error for email already registered
+                echo"
+                    <script>
+                        alert('$result_fetch[email] - Is Already Registered');
+                        window.location.href = 'index.php'; 
+                    </script>
+                ";
+            }
+        }
+        else # this query will be executed when no one has taken username or email before
+        {
+            $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+            $query = "INSERT INTO `registered_users`(`full_name`, `user_name`, `email`, `password`) VALUES ('$_POST[fullname]','$_POST[username]','$_POST[email]','$password')";
+
+            if(mysqli_query($conn, $query)) {
+                # if data inserted successfully
+                echo"
+                    <script>
+                        alert('Registration Successful');
+                        window.location.href = 'index.php'; 
+                    </script>
+                ";
+            } 
+            else {
+                # if data cannot be inserted
+                echo"
+                    <script>
+                        alert('Cannot Run Query');
+                        window.location.href = 'index.php'; 
+                    </script>
+                ";
+            }
+        }
+    }
+    else {
+        echo"
+            <script>
+                alert('Cannot Run Query');
+                window.location.href = 'index.php'; 
+            </script>
+        ";
+    }
+}
+
+
+?>
