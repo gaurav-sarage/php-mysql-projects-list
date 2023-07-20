170a58f14652d7bd50740e3f68fe8fb35e8219ae
Gaurav <gauravsarage.1902@gmail.com>
2023-07-21 00:41:13 +0530

    added basic code files to setup composer and webrtc in the php files

--- sleekSession/index.php
+++ sleekSession/index.php
@@ -1,3 +1,14 @@
 <?php
     echo "Hello World";
 ?>
+
+<script>
+    var conn = new WebSocket('ws://localhost:8080');
+    conn.onopen = function(e) {
+        console.log("Connection established!");
+    };
+
+    conn.onmessage = function(e) {
+        console.log(e.data);
+    };
+</script>
